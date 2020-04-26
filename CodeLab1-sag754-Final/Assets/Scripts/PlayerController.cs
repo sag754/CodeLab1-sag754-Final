1325a7ae9d1015e17a6dc026ee10007859824d29
Stephen A. Gonzalez <sag754@nyu.edu>
2020-04-26 18:54:51 -0400

    Added the ability to shoot and some animations.

---
+++ CodeLab1-sag754-Final/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public KeyCode moveUp;
+    public KeyCode moveDown;
+    public KeyCode moveForward;
+    public KeyCode moveBack;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(moveUp))
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(0, 5);
+        }
+
+        if (Input.GetKey(moveForward))
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(5, 0);
+        }
+
+        if (Input.GetKey(moveDown))
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(0, -5);
+        }
+
+        if (Input.GetKey(moveBack))
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(-5, 0);
+        }
+
+        if ((!Input.GetKey(moveDown)) && (!Input.GetKey(moveUp)) && (!Input.GetKey(moveForward)) && (!Input.GetKey(moveBack)))
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(0, 0);
+        }
+    }
+}
---
+++ CodeLab1-sag754-Final/Assets/Scripts/Weapon.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class Weapon : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Transform firepoint;
+    public GameObject bulletPrefab;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        Instantiate(bulletPrefab, firepoint.position, firepoint.rotation);
+    }
+}
