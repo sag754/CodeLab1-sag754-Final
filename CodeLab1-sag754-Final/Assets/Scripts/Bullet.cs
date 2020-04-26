36a54d339a5102e9f7d60a6d9fc1d9dcffb175ee
Stephen A. Gonzalez <sag754@nyu.edu>
2020-04-26 19:23:18 -0400

    Added enemy health and death sequence.

--- CodeLab1-sag754-Final/Assets/Scripts/Bullet.cs
+++ CodeLab1-sag754-Final/Assets/Scripts/Bullet.cs
@@ -5,8 +5,9 @@ using UnityEngine;
 public class Bullet : MonoBehaviour
 {
     public float speed = 20f;
-    public int damage = 100;
+    public int damage = 10;
     public Rigidbody2D rb;
+    public GameObject hitEffect;
 
     // Start is called before the first frame update
     void Start()
@@ -14,8 +15,16 @@ public class Bullet : MonoBehaviour
         rb.velocity = transform.right * speed;
     }
 
-    void OnTriggerEnter2D(Collider2D collider)
+    void OnTriggerEnter2D(Collider2D hitInfo)
     {
+        Enemy enemy = hitInfo.GetComponent<Enemy>();
+        if(enemy != null)
+        {
+            enemy.TakeDamage(damage);
+        }
+
+        Instantiate(hitEffect, transform.position, transform.rotation);
+
         Destroy(gameObject);
     }
 }
